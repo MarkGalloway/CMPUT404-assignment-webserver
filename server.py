934e04b1609d325b0ae162da7f2a026607d68ff0
Tamara Bain <tamarabyte@gmail.com>
2015-01-19 13:07:20 -0700

    Added images, updated liscence

--- server.py
+++ server.py
@@ -1,7 +1,27 @@
+# coding: utf-8
+
+# Copyright 2015 Abram Hindle, Eddie Antonio Santos, Tamara Bain, Mark Galloway
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# Furthermore it is derived from the Python documentation examples thus
+# some of the code is Copyright 2001-2013 Python Software
+# Foundation; All Rights Reserved
+
 import SocketServer
 import os
 import email.utils
-
 
 class MyWebServer(SocketServer.BaseRequestHandler):
 
